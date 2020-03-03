3114ec41069fcec95f0a3543216207a2f2144c4e
Eric Denovellis <edeno@bu.edu>
2020-03-02 16:11:51 -0800

    Make more pythonic.

--- lesion_similarity_analysis/core.py
+++ lesion_similarity_analysis/core.py
@@ -1,76 +1,84 @@
 import time
+from itertools import combinations
 
 import numpy as np
 from skimage import io
 from skimage.metrics import structural_similarity as ssim
 
-anKey = np.array([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
-                  111, 112, 113, 114, 115,
-                  200, 201, 202, 203, 204, 205, 206, 207, 208])
+animals = np.array([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+                    111, 112, 113, 114, 115,
+                    200, 201, 202, 203, 204, 205, 206, 207, 208])
 
 control = np.array([0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0,
                     1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0])
 
+output_size = {
+    ("L", "Dorsal"): 363,
+    ("L", "Intermediate"): 498,
+    ("R", "Dorsal"): 349,
+    ("R", "Intermediate"): 500,
+}
 
-def evaluateAllSlice(LorR, hippoPart, sig):
-    fName = "Desktop/sim_" + LorR + "_" + hippoPart + ".txt"
+
+def evaluateAllSlice(LorR, hippoPart, sigma):
+    """
+
+    Parameters
+    ----------
+    LorR : ("L", "R")
+        Brain hemisphere
+    hippoPart : ("Dorsal", "Intermediate")
+        Sub area of the hippocampus
+    sigma : float
+        Gaussian standard deviation
+
+    """
     duration = 0
-    if(LorR == "L"):
-        if(hippoPart == "Dorsal"):
-            output = np.zeros((anKey.size, anKey.size, 363))
-        elif(hippoPart == "Intermediate"):
-            output = np.zeros((anKey.size, anKey.size, 498))
-    elif(LorR == "R"):
-        if(hippoPart == "Dorsal"):
-            output = np.zeros((anKey.size, anKey.size, 349))
-        elif(hippoPart == "Intermediate"):
-            output = np.zeros((anKey.size, anKey.size, 500))
-    howMany = output.shape[2]
-    for i in range(howMany):
-        if(i < 10):
-            which = "000" + str(i)
-        elif(i < 100):
-            which = "00" + str(i)
-        elif(i < 1000):
-            which = "0" + str(i)
+    n_slices = output_size[(LorR, hippoPart)]
+    output = np.zeros((animals.size, animals.size, n_slices))
+
+    for i in range(n_slices):
+        if i < 10:
+            which = f"000{i}"
+        elif i < 100:
+            which = f"00{i}"
+        elif i < 1000:
+            which = f"0{i}"
         else:
             which = str(i)
         start = time.time()
-        sim = getAllSim(which, LorR, hippoPart, sig)
+        sim = getAllSim(which, LorR, hippoPart, sigma)
         fullSim = completeArray(sim)
         output[:, :, i] = fullSim[:, :]
         end = time.time()
+
         duration *= i
         duration += (end - start)
         duration /= (i + 1)
         print(duration)
-        print(((howMany - i - 1) * duration) / 60)
-    f = open(fName, "w+")
-    f.write(np.array2string(output))
-    f.close
+        print(((n_slices - i - 1) * duration) / 60)
+
+    output_filename = f"Desktop/sim_{LorR}_{hippoPart}.txt"
+    with open(output_filename, "w+") as file:
+        file.write(np.array2string(output))
 
 
-def getAllSim(which, LorR, hippoPart, sig):
-    sim = np.zeros((anKey.size, anKey.size))
-    for i in range(anKey.size):
-        for j in range(anKey.size):
-            if(i > j):
-                sim[i][j] = getSim(str(anKey[i]), str(
-                    anKey[j]), which, LorR, hippoPart, sig)
-            else:
-                sim[i][j] = np.nan
+def getAllSim(which, LorR, hippoPart, sigma):
+    sim = np.full((animals.size, animals.size), np.nan)
+    for i, j in combinations(range(animals.size), 2):
+        sim[i][j] = getSim(str(animals[i]), str(
+            animals[j]), which, LorR, hippoPart, sigma)
     return sim
 
 
-def getSim(an1, an2, which, LorR, hippoPart, sig):
+def getSim(an1, an2, which, LorR, hippoPart, sigma):
     im1 = loadImage(an1, which, LorR, hippoPart)
     im2 = loadImage(an2, which, LorR, hippoPart)
-    return ssim(im1, im2, gaussian_weights=True, sigma=sig)
+    return ssim(im1, im2, gaussian_weights=True, sigma=sigma)
 
 
 def loadImage(nm, which, LorR, hippoPart):
-    imageStr = "Desktop/FXScoh4/Cropped/" + hippoPart + "/" + \
-        LorR + "/" + nm + "/" + nm + LorR + which + ".tif"
+    imageStr = f"Desktop/FXScoh4/Cropped/{hippoPart}/{LorR}/{nm}/{nm}/{nm}{LorR}{which}.tiff"
     image = io.imread(imageStr)
     return image
 
@@ -79,7 +87,7 @@ def getNN(input):
     NNinfo = np.zeros((input.shape[0], 2))
     for i in range(control.size):
         for j in range(control.size):
-            if(control[j]):
+            if control[j]:
                 if(NNinfo[i][0] < input[i][j]):
                     NNinfo[i][0] = input[i][j]
                     NNinfo[i][1] = control[j]
@@ -87,9 +95,9 @@ def getNN(input):
 
 
 def completeArray(input):
-    fullSim = np.zeros((anKey.size, anKey.size))
-    for i in range(anKey.size):
-        for j in range(anKey.size):
+    fullSim = np.zeros((animals.size, animals.size))
+    for i in range(animals.size):
+        for j in range(animals.size):
             if(i < j):
                 fullSim[i][j] = input[j][i]
             else:
