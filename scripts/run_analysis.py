46f6620662eaa0f74be5f8f6b5360e1dc35a048b
Eric Denovellis <edeno@bu.edu>
2020-05-15 14:11:21 -0700

    Remove LorR

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -5,9 +5,9 @@ from lesion_similarity_analysis.core import SLICE_SIZE, evaluate_slices
 
 def main():
     data_path = "/data2/data1_backup/david/FXS/FXScoh3/LesionEvaluation/FXScoh3"
-    for LorR, hippoPart in SLICE_SIZE:
-        print(f"Evaluating {hippoPart}, {LorR}")
-        evaluate_slices(data_path, hippoPart, LorR, sigma=50,
+    for hippoPart in SLICE_SIZE:
+        print(f"Evaluating {hippoPart}")
+        evaluate_slices(data_path, hippoPart, sigma=50,
                         output_path="../PROCESSED_DATA")
 
 
